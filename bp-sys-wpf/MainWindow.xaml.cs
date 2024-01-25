21c78838b40cd6e97b4800d96cce2ba83abe0bb2
PLFJY <gxpjy2008@163.com>
2024-01-26 01:04:48 +0800

    增加比分模块的前端界面，后端逻辑未写

--- bp-sys-wpf/MainWindow.xaml.cs
+++ bp-sys-wpf/MainWindow.xaml.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.Win32;
 using System.Diagnostics;
+using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Timers;
+using System.Linq;
 
 namespace bp_sys_wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public static MainWindow mainWindow;
@@ -19,6 +25,8 @@ namespace bp_sys_wpf
         public string[] main_team_player_list = new string[8], away_team_player_list = new string[8];
         public bool[] main_team_player_state = new bool[8], away_team_player_state = new bool[8];
         public int count_main_sur = 0, count_away_sur = 0, count_main_hun = 0, count_away_hun = 0;
+        private DispatcherTimer dispatcherTimer;
+        private int countdownTime;
         private string GetFilePath(string type, string selectedValue)
         {
             string temp = "pack://application:,,,/pic/" + type + "/";
@@ -412,6 +420,61 @@ namespace bp_sys_wpf
             (Now_sur_player_4.Text, Now_sur_player_2.Text) = (Now_sur_player_2.Text, Now_sur_player_4.Text);
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Countdown_set.Text, out int number))
+            {
+                Front.front.timmer.Content = Countdown_set.Text;
+                countdownTime = Convert.ToInt32(Countdown_set.Text);
+                dispatcherTimer.Start();
+            }
+            else
+            {
+                Countdown_set.Text = null;
+            }
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            countdownTime--;
+            if (countdownTime <= 0)
+            {
+                dispatcherTimer.Stop();
+            }
+            else
+            {
+                Front.front.timmer.Content = countdownTime.ToString();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Stop();
+            Front.front.timmer.Content = "VS";
+        }
+        bool IsWindowOpen(string windowName)
+        {
+            return Application.Current.Windows.OfType<Window>().Any(window => window.Name == windowName);
+        }
+
+        private void Open_score_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsWindowOpen("Score1"))
+            {
+                Score score = new Score();
+                score.Show();
+            }
+            if (!IsWindowOpen("ScoreSur1"))
+            {
+                ScoreSur scoreSur = new ScoreSur();
+                scoreSur.Show();
+            }
+            if (!IsWindowOpen("ScoreHun1"))
+            {
+                ScoreHun scoreHun = new ScoreHun();
+                scoreHun.Show();
+            }
+        }
+
         private void Swap_sur_player4_with_player3_Click(object sender, RoutedEventArgs e)
         {
             (Now_sur_player_4.Text, Now_sur_player_3.Text) = (Now_sur_player_3.Text, Now_sur_player_4.Text);
@@ -476,47 +539,47 @@ namespace bp_sys_wpf
             Sur_hole_ban_5.SelectedItem = null;
             Sur_hole_ban_6.SelectedItem = null;
             // image rest
-            this.Hun_ban_review_1.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            Front.front.Hun_ban_1.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            this.Hun_ban_review_2.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            Front.front.Hun_ban_2.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            this.Hun_ban_review_3.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            Front.front.Hun_ban_3.Source = new BitmapImage(new Uri(GetFilePath("hunban", "null")));
-            this.Hun_pick_review.Source = new BitmapImage(new Uri(GetFilePath("hun", "null")));
-            Front.front.Hun_pick.Source = new BitmapImage(new Uri(GetFilePath("hunhalf", "null")));
-            Interlude.interlude.Hun.Source = new BitmapImage(new Uri(GetFilePath("hunBig", "null")));
-            this.Sur_pick_1_preview.Source = new BitmapImage(new Uri(GetFilePath("sur", "null")));
-            Front.front.Sur_pick_1.Source = new BitmapImage(new Uri(GetFilePath("surhalf", "null")));
-            Interlude.interlude.Sur_1.Source = new BitmapImage(new Uri(GetFilePath("surBig", "null")));
-            this.Sur_pick_2_preview.Source = new BitmapImage(new Uri(GetFilePath("sur", "null")));
-            Front.front.Sur_pick_2.Source = new BitmapImage(new Uri(GetFilePath("surhalf", "null")));
-            Interlude.interlude.Sur_2.Source = new BitmapImage(new Uri(GetFilePath("surBig", "null")));
-            this.Sur_pick_3_preview.Source = new BitmapImage(new Uri(GetFilePath("sur", "null")));
-            Front.front.Sur_pick_3.Source = new BitmapImage(new Uri(GetFilePath("surhalf", "null")));
-            Interlude.interlude.Sur_3.Source = new BitmapImage(new Uri(GetFilePath("surBig", "null")));
-            this.Sur_pick_4_preview.Source = new BitmapImage(new Uri(GetFilePath("sur", "null")));
-            Front.front.Sur_pick_4.Source = new BitmapImage(new Uri(GetFilePath("surhalf", "null")));
-            Interlude.interlude.Sur_4.Source = new BitmapImage(new Uri(GetFilePath("surBig", "null")));
-            this.Sur_ban_review_1.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Sur_ban_1.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_ban_review_2.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Sur_ban_2.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_ban_review_3.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Sur_ban_3.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_ban_review_4.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Sur_ban_4.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_1_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_1.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_2_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_2.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_3_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_3.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_4_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_4.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_5_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_5.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            this.Sur_hole_ban_6_preview.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
-            Front.front.Hole_ban_6.Source = new BitmapImage(new Uri(GetFilePath("surban", "null")));
+            this.Hun_ban_review_1.Source = null;
+            Front.front.Hun_ban_1.Source = null;
+            this.Hun_ban_review_2.Source = null;
+            Front.front.Hun_ban_2.Source = null;
+            this.Hun_ban_review_3.Source = null;
+            Front.front.Hun_ban_3.Source = null;
+            this.Hun_pick_review.Source = null;
+            Front.front.Hun_pick.Source = null;
+            Interlude.interlude.Hun.Source = null;
+            this.Sur_pick_1_preview.Source = null;
+            Front.front.Sur_pick_1.Source = null;
+            Interlude.interlude.Sur_1.Source = null;
+            this.Sur_pick_2_preview.Source = null;
+            Front.front.Sur_pick_2.Source = null;
+            Interlude.interlude.Sur_2.Source = null;
+            this.Sur_pick_3_preview.Source = null;
+            Front.front.Sur_pick_3.Source = null;
+            Interlude.interlude.Sur_3.Source = null;
+            this.Sur_pick_4_preview.Source = null;
+            Front.front.Sur_pick_4.Source = null;
+            Interlude.interlude.Sur_4.Source = null;
+            this.Sur_ban_review_1.Source = null;
+            Front.front.Sur_ban_1.Source = null;
+            this.Sur_ban_review_2.Source = null;
+            Front.front.Sur_ban_2.Source = null;
+            this.Sur_ban_review_3.Source = null;
+            Front.front.Sur_ban_3.Source = null;
+            this.Sur_ban_review_4.Source = null;
+            Front.front.Sur_ban_4.Source = null;
+            this.Sur_hole_ban_1_preview.Source = null;
+            Front.front.Hole_ban_1.Source = null;
+            this.Sur_hole_ban_2_preview.Source = null;
+            Front.front.Hole_ban_2.Source = null;
+            this.Sur_hole_ban_3_preview.Source = null;
+            Front.front.Hole_ban_3.Source = null;
+            this.Sur_hole_ban_4_preview.Source = null;
+            Front.front.Hole_ban_4.Source = null;
+            this.Sur_hole_ban_5_preview.Source = null;
+            Front.front.Hole_ban_5.Source = null;
+            this.Sur_hole_ban_6_preview.Source = null;
+            Front.front.Hole_ban_6.Source = null;
         }
         public MainWindow()
         {
@@ -533,6 +596,9 @@ namespace bp_sys_wpf
             interlude.Show();
             Map_bp map_Bp = new Map_bp();
             map_Bp.Show();
+            dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
         }
         public string OpenImageFileDialog()//打开通用对话框选取图片
         {
@@ -622,7 +688,7 @@ namespace bp_sys_wpf
                 Front.front.Sur_2_team.Content = Away_team_name.Text;
                 Front.front.Sur_3_team.Content = Away_team_name.Text;
                 Front.front.Sur_4_team.Content = Away_team_name.Text;
-                int flag1=1, flag2=1, flag3=1, flag4=1, flag5=1;
+                int flag1 = 1, flag2 = 1, flag3 = 1, flag4 = 1, flag5 = 1;
                 for (int i = 0, j = 1; i < 6; i++)
                 {
                     if (away_team_player_state[i] == true)
@@ -678,52 +744,51 @@ namespace bp_sys_wpf
                 Front.front.Sur_2_team.Content = Main_team_name.Text;
                 Front.front.Sur_3_team.Content = Main_team_name.Text;
                 Front.front.Sur_4_team.Content = Main_team_name.Text;
-                bool flag = true;
+                int flag1 = 1, flag2 = 1, flag3 = 1, flag4 = 1, flag5 = 1;
                 for (int i = 0, j = 1; i < 6; i++)
                 {
                     if (main_team_player_state[i] == true)
                     {
-                        flag = false;
                         switch (j)
                         {
                             case 1:
                                 Now_sur_player_1.Text = main_team_player_list[i];
                                 j++;
+                                flag1 = 0;
                                 break;
                             case 2:
                                 Now_sur_player_2.Text = main_team_player_list[i];
                                 j++;
+                                flag2 = 0;
                                 break;
                             case 3:
                                 Now_sur_player_3.Text = main_team_player_list[i];
                                 j++;
+                                flag3 = 0;
                                 break;
                             case 4:
                                 Now_sur_player_4.Text = main_team_player_list[i];
                                 j++;
+                                flag4 = 0;
                                 break;
                         }
                     }
                 }
-                if (flag == true)
-                {
-                    Now_sur_player_1.Text = null;
-                    Now_sur_player_2.Text = null;
-                    Now_sur_player_3.Text = null;
-                    Now_sur_player_4.Text = null;
-                }
-                flag = true;
+                if (flag1 == 1) Now_sur_player_1.Text = null;
+                if (flag2 == 1) Now_sur_player_2.Text = null;
+                if (flag3 == 1) Now_sur_player_3.Text = null;
+                if (flag4 == 1) Now_sur_player_4.Text = null;
                 if (away_team_player_state[6] == true)
                 {
                     Now_hun_player.Text = away_team_player_list[6];
-                    flag = false;
+                    flag5 = 0;
                 }
                 if (away_team_player_state[7] == true)
                 {
                     Now_hun_player.Text = away_team_player_list[7];
-                    flag = false;
+                    flag5 = 0;
                 }
-                if (flag == true) Now_hun_player.Text = null;
+                if (flag5 == 1) Now_hun_player.Text = null;
             }
         }
     }
---
+++ bp-sys-wpf/ScoreSur.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace bp_sys_wpf
+{
+    /// <summary>
+    /// ScoreSur.xaml 的交互逻辑
+    /// </summary>
+    public partial class ScoreSur : Window
+    {
+        public static ScoreSur scoreSur;
+        public ScoreSur()
+        {
+            InitializeComponent();
+            scoreSur = this;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}

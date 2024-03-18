2bd0c1bf4b753606e5abdcf000fb45ac833683ff
bili PLFJY <gxpjy2008@163.com>
2024-03-18 23:23:57 +0800

    定义了一个config用于储存当前版本 改了一下aboutthis的UI以便自动更新

---
+++ bp-sys-wpf/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bp_sys_wpf
+{
+    public static class Config
+    {
+        public static string version { get; set; } = "1.7";
+    }
+}

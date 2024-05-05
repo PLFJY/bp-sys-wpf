712938161f00127d653a63c593f028fa03336921
PLFJY <gxpjy2008@163.com>
2024-05-05 21:50:56 +0800

    V2.5 加入新监管者“跛脚羊”，新求生者“骑士”和"法罗女士"

--- bp-sys-wpf/Config.cs
+++ bp-sys-wpf/Config.cs
@@ -4,7 +4,7 @@ namespace bp_sys_wpf
 {
     public static class Config
     {
-        public static string version { get; set; } = "V2.4";
+        public static string version { get; set; } = "V2.5";
         public static bool Border { get; set; }
         public static class Front
         {

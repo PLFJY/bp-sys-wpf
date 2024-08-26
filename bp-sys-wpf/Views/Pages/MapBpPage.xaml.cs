92146137782e7d7c80f4015f37ec3a2a5e886eed
PLFJY <gxpjy2008@163.com>
2024-08-27 01:50:03 +0800

    回滚至V3.1.2-SHISL，并做了一些修改 前台和过场顶上的队名单独设置

--- bp-sys-wpf/Views/Pages/MapBpPage.xaml.cs
+++ bp-sys-wpf/Views/Pages/MapBpPage.xaml.cs
@@ -13,5 +13,20 @@ namespace bp_sys_wpf.Views.Pages
             InitializeComponent();
             DataContext = BackWindow.backWindow.rootViewModel;
         }
+
+        private void GameProgressCheck_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Front.front.GameProgress.Text = GameProgressEdit.Text;
+            }
+            catch
+            {
+                BackWindow.backWindow.MessageBar.IsOpen = true;
+                BackWindow.backWindow.MessageBar.Title = "错误";
+                BackWindow.backWindow.MessageBar.Severity = Wpf.Ui.Controls.InfoBarSeverity.Error;
+                BackWindow.backWindow.MessageBar.Message = "请先启动前台";
+            }
+        }
     }
 }

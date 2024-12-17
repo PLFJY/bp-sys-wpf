33d34a67562729f0ff7b39aa736b2843a6c66fc8
PLFJY <gxpjy2008@163.com>
2024-12-17 08:39:58 +0800

    完成ScoreHun的适配

--- bp-sys-wpf/Views/Windows/ScoreHun.xaml.cs
+++ bp-sys-wpf/Views/Windows/ScoreHun.xaml.cs
@@ -24,7 +24,7 @@ namespace bp_sys_wpf
             S.Foreground = Config.Score.Color.S;
             Win.Foreground = Config.Score.Color.Score;
             Lose.Foreground = Config.Score.Color.Score;
-            All.Foreground = Config.Score.Color.Score;
+            Tie.Foreground = Config.Score.Color.Score;
             WinWord.Foreground = Config.Score.Color.Word;
             LoseWord.Foreground = Config.Score.Color.Word;
             AllWord.Foreground = Config.Score.Color.Word;

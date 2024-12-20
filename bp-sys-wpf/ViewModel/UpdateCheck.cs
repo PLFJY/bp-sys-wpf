2411839b0543d6f81fdbec9d195e6d826bce4fdf
PLFJY <gxpjy2008@163.com>
2024-12-21 01:33:07 +0800

    彻底解决了连接不上Github服务器导致软件崩溃的问题（其实就是try套错位置了）

--- bp-sys-wpf/ViewModel/UpdateCheck.cs
+++ bp-sys-wpf/ViewModel/UpdateCheck.cs
@@ -18,31 +18,28 @@ namespace bp_sys_wpf.ViewModel
             repository = "plfjy/bp-sys-wpf-update";
             releasesUrl = $"{baseUrl}/repos/{repository}/releases/latest";
             string userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/116.0";
-            using (HttpClient client = new())
+            string responseJson;
+            try
             {
-                client.DefaultRequestHeaders.Add("User-Agent", userAgent);
-                client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
-                client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
-                try
+                using (HttpClient client = new())
                 {
+                    client.DefaultRequestHeaders.Add("User-Agent", userAgent);
+                    client.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+                    client.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.5");
                     // 发起GET请求并获取JSON响应内容
                     HttpResponseMessage response = await client.GetAsync(releasesUrl);
                     response.EnsureSuccessStatusCode();
-                    var responseJson = await response.Content.ReadAsStringAsync();
-                    // 使用System.Text.Json进行反序列化
-                    releaseInfo = System.Text.Json.JsonSerializer.Deserialize<GiteeReleaseInfo>(responseJson);
-                    Issuccessful = true;
+                    responseJson = await response.Content.ReadAsStringAsync();
+                    
                 }
-                catch (FlurlHttpException ex)
-                {
-                    Debug.WriteLine($"请求失败: {ex.Message}");
-                    Issuccessful = false;
-                }
-                catch (JsonException jex)
-                {
-                    Debug.WriteLine($"JSON解析失败: {jex.Message}");
-                    Issuccessful = false;
-                }
+                // 使用System.Text.Json进行反序列化
+                releaseInfo = JsonSerializer.Deserialize<GiteeReleaseInfo>(responseJson);
+                Issuccessful = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"请求失败: {ex.Message}");
+                Issuccessful = false;
             }
         }
     }
